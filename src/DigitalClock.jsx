dcb715383d5e70458c0bb05bf17bad5cba099904
Gladys <gladysalu1@gmail.com>
2024-02-22 00:07:19 +0100

    commit

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+
+const DigitalClock = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+            setTime(new Date())
+        }, 1000);
+        return()=>{
+            clearInterval(intervalId)
+        }
+    }, []);
+    function formatTime(){
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridian = hours >= 12 ? "PM" : "AM"
+
+        hours = hours % 12 || 12;
+        return `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)} ${meridian}`
+    }
+    function addZero(number){
+        return (number < 10 ? "0" : "") + number;
+    }
+  return (
+    <div className="container flex justify-center min-h-[100vh] items-center bg-slate-950 min-w-[100vw]">
+        <div className="text-8xl font-bold font-mono text-shadow-md  text-white backdrop-brightness-150">
+            <span>{formatTime()}</span>
+        </div>
+    </div>
+  )
+}
+
+export default DigitalClock
